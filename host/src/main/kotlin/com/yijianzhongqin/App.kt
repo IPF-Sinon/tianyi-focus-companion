bc5be73b56bb5d54a068a3beeee1a3f4033d4500
PolarNight <ipf_sinon@163.com>
2026-08-23 20:34:55 +0800

    Add host/src/main/kotlin/com/yijianzhongqin/App.kt

---
+++ host/src/main/kotlin/com/yijianzhongqin/App.kt
@@ -0,0 +1,32 @@
+package com.yijianzhongqin
+
+import android.app.Application
+import com.yijianzhongqin.shell.PluginManager
+import dagger.hilt.android.HiltAndroidApp
+
+/**
+ * 依见钟勤 - 应用入口
+ */
+@HiltAndroidApp
+class App : Application() {
+
+    lateinit var pluginManager: PluginManager
+        private set
+
+    override fun onCreate() {
+        super.onCreate()
+        instance = this
+        pluginManager = PluginManager(this)
+        pluginManager.initialize()
+    }
+
+    override fun onTerminate() {
+        pluginManager.shutdownBlocking()
+        super.onTerminate()
+    }
+
+    companion object {
+        lateinit var instance: App
+            private set
+    }
+}
