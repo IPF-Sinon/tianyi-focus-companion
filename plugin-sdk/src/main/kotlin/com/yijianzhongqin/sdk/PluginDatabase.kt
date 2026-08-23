1d6316c7cdfb7bc7943e4461a0c57fce93ef9fdc
PolarNight <ipf_sinon@163.com>
2026-08-23 20:39:47 +0800

    Add plugin-sdk/src/main/kotlin/com/yijianzhongqin/sdk/PluginDatabase.kt

---
+++ plugin-sdk/src/main/kotlin/com/yijianzhongqin/sdk/PluginDatabase.kt
@@ -0,0 +1,13 @@
+package com.yijianzhongqin.sdk
+
+import androidx.room.RoomDatabase
+
+/**
+ * 插件私有数据库。
+ * 每个插件通过此接口获取自己的 Room 数据库实例。
+ * 插件不直接创建 Room 数据库，而是通过宿主统一管理。
+ */
+interface PluginDatabase {
+    fun <T : RoomDatabase> getDatabase(dbClass: Class<T>): T
+    fun close()
+}
