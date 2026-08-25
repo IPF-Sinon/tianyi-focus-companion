3935dd5db4b8ea214d8ff4532173156a8d13dce6
PolarNight <ipf_sinon@163.com>
2026-08-25 23:38:38 +0000

    Fix: remove old com.yijianzhongqin package duplicates
    Hilt AggregrateDeps fails with "Cannot process multiple app roots:
    com.yijianzhongqin.App and top.funcun.companion.App". Delete all
    old package sources from host, plugin-sdk, and plugins modules.

--- plugin-sdk/src/main/kotlin/com/yijianzhongqin/sdk/util/ServiceToken.kt
+++
@@ -1,8 +0,0 @@
-package com.yijianzhongqin.sdk.util
-
-/** 服务注册/发现 Token */
-class ServiceToken<T>(val name: String) {
-    companion object {
-        fun <T> create(name: String): ServiceToken<T> = ServiceToken(name)
-    }
-}
